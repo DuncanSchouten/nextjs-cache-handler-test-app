48ca0489acb14812ac3d9e1096863539440c873c
Kevin Porras <kevin.porras@pantheon.io>
2026-02-06 16:21:41 -0600

    Update route.ts

--- app/api/posts/with-tags/route.ts
+++ app/api/posts/with-tags/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { fetchPostsWithTags } from '../../../../lib/blogService';
 import { unstable_cacheTag as cacheTag } from 'next/cache'
-'use cache'
 
 export async function GET(request: NextRequest) {
   'use cache'
